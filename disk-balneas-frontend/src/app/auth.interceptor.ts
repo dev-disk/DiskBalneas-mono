2898fe5bd2e70dbdf70aa73455e97f9719e80cf6
KaykySiq <josekayky@hotmail.com>
2025-02-22 00:05:51 -0300

    fix(DEV-32): Protege as rotas.

---
+++ disk-balneas-frontend/src/app/auth.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Verifica se a URL pertence aos endpoints de login
+    if (req.url.includes('/login')) {
+      return next.handle(req);
+    }
+
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
